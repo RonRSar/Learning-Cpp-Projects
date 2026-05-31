dcd3fea5a01dc91eee68edb4bff26c72e8dceabf
RonRSar <ronitsarna@gmail.com>
2026-05-31 21:30:43 +0800

    revisit horseplay to add more optimal implementation

--- 2_Level_Up_Challenges/ch06_knight_moves/ch6_horseplay.cpp
+++ 2_Level_Up_Challenges/ch06_knight_moves/ch6_horseplay.cpp
@@ -17,9 +17,8 @@
 //           knight: The knight's location.
 // Returns: An STL vector of strings with the possible locations to move.
 
-// This is very inefficient! I plan to revisit this in the future. I think my mistake was
-// not abstracting the problem down to numbers and working with the string notation of the 
-// chessboard directly 
+// This was very inefficient! I think my mistake was not abstracting the problem down
+// working with the string notation of the chessboard directly
 std::vector<std::string> knight_moves(std::string knight)
 {
     std::vector<std::string> moves;
@@ -38,7 +37,7 @@ std::vector<std::string> knight_moves(std::string knight)
     std::vector<std::string> possible_moves{};
 
     // Check every valid position before adding to moves. Inefficient (O(8)?) but works c:
-    // Potential improvement would be to abstract this to a loop. 
+    // Potential improvement would be to abstract this to a loop.
     if (std::find(alpha_grid.begin(), alpha_grid.end(), *(alpha_it + 2)) != alpha_grid.end())
     {
         if (std::find(num_grid.begin(), num_grid.end(), *(num_it + 1)) != num_grid.end())
@@ -74,9 +73,54 @@ std::vector<std::string> knight_moves(std::string knight)
     return moves;
 }
 
+// This function was my attempt to optimise it so its more readable and less verbose
+// no need for a vector for the individual grids. also no need for possible_moves vector.
+// im sure there is perhaps room for even smaller code (like by writing out the move matrix)
+// but im satisfied with this solution and think its an improvement over the last one.  
+std::vector<std::string> optimal_imp_knight_moves(std::string knight)
+{
+    std::vector<std::string> moves{};
+    char k_alpha{knight[0]};
+    char k_num{knight[1]};
+
+    const char delta_i[2] = {-2, 2};
+    const char delta_j[2] = {-1, 1};
+
+    auto find_knight_moves = [&moves, &delta_i, &delta_j](char grid_alpha, char grid_num, bool flip)
+    {
+        char new_k_alpha{}, new_k_num{};
+        for (auto i : delta_i)
+        {
+            for (auto j : delta_j)
+            {
+                if (flip)
+                {
+                    new_k_alpha = grid_alpha + i;
+                    new_k_num = grid_num + j;
+                }
+                else
+                {
+                    new_k_alpha = grid_alpha + j;
+                    new_k_num = grid_num + i;
+                }
+
+                if  (new_k_alpha >= 'a' && new_k_alpha <= 'h' && new_k_num >= '1' && new_k_num <= '8')
+                {
+                    moves.push_back(std::string(1, new_k_alpha) + std::string(1, new_k_num));
+                }
+            }
+        }
+    };
+
+    find_knight_moves(k_alpha, k_num, true); //true means add 2 to alpha first then 1 to num 
+    find_knight_moves(k_alpha, k_num, false); //false means opposite
+
+    return moves;
+}
+
 bool check_moves(const std::string &knight, const std::vector<std::string> &expected)
 {
-    std::vector<std::string> result = knight_moves(knight);
+    std::vector<std::string> result = optimal_imp_knight_moves(knight);
     if (result.size() != expected.size())
         return false;
     std::vector<std::string> r = result;
